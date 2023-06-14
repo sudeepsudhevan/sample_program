95f70b3399053b2740432964ffa9f064dea0be0c
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-14 13:27:27 +0530

    flutter container and alignment

--- lib/main.dart
+++ lib/main.dart
@@ -28,23 +28,101 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       //appBar: AppBar(),
       body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(children: [
-            Text(
-              name,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
-              ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.red,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      name,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          print('Text Button Clicked');
+                        },
+                        child: Text('Click me')),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              print('Elevated Button Clicked');
+                            },
+                            child: Text('Click me')),
+                        IconButton(
+                            onPressed: () {
+                              print('Icon Button Clicked');
+                            },
+                            icon: Icon(Icons.mic)),
+                      ],
+                    ),
+                  ]),
             ),
-            TextButton(onPressed: () {}, child: Text('Click me'))
-          ]),
-        ),
-      ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(
+                  color: Color.fromARGB(255, 5, 53, 158),
+                  width: 5,
+                ),
+              ),
+              child: Center(child: Text('you are hacked')),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      name,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          print('Text Button Clicked');
+                        },
+                        child: Text('Click me')),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              print('Elevated Button Clicked');
+                            },
+                            child: Text('Click me')),
+                        IconButton(
+                            onPressed: () {
+                              print('Icon Button Clicked');
+                            },
+                            icon: Icon(Icons.mic)),
+                      ],
+                    ),
+                  ]),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
